ede2c938c6f1fbed2c2bc6a1af6dd410d870de0e
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2017-09-22 23:38:00 -0500

    Update test.php

--- ws/test.php
+++ ws/test.php
@@ -1,2 +1,19 @@
 <?php
+if(!empty($GET['name']){
+  $name=$_GET['name'];
+  $price=get_price($name);
+  
+if(empty($price))
+  //echo:"Book not found";
+}else{
+  //respond book price
+}else{
+  //throw invalid request
+}
+   function deliver_response($status,$status_message,$data){
+   header("HTTP/1.1 $status $status_message");
+     $response['status']=$status;
+     $response['status_message']=$status_message;
+     $response['data']=$data;
+   }
 ?>
