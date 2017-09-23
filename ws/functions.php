86a5a10e3f6e374917f1e0eb8e4192099e4c4ac1
Luis Fernando Gutiérrez Romo <changoluis@hotmail.com>
2017-09-22 23:56:52 -0500

    Update functions.php

--- ws/functions.php
+++ ws/functions.php
@@ -1,1 +1,15 @@
-
+<?php
+function get_price($find){
+  $books=array(
+  "java"=>299,
+  "c"=>120,
+  "php"=>123
+  );
+  foreach($books as $book=>$price){
+    if($book==$find){
+      return $price;
+    break;
+    }
+  }
+}
+?>
