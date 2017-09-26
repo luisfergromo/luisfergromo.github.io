509cc64191b5e5d0ccda7fcef9e2e02e421e0db5
luisromo <changoluis@hotmail.com>
2017-09-25 21:29:12 -0500

    html index & test.ts (TypeScript)

--- ws/test.js
+++ ws/test.js
@@ -7,14 +7,29 @@ var message = "Hello Wold"
 console.log(message)
 document.body.innerHTML = message;
 */
-var message = "hello world";
-console.log(message);
-document.body.innerHTML = message;
+/*
+var message = "hello world"
+console.log(message)
+document.body.innerHTML= message
+
+var req = new XMLHttpRequest()
+
+req.onload= function (e) {
+
+    var response = req.response;
+    console.log(response)
+}
+
+req.open("GET","https://api.github.com/users/luisfergromo/repos")
+req.responseType="json"
+req.send()
+*/
 var req = new XMLHttpRequest();
 req.onload = function (e) {
     var response = req.response;
     console.log(response);
+    console.log();
 };
-req.open("GET", "https://api.github.com/users/luisfergromo/repos");
+req.open("GET", "https://maps.googleapis.com/maps/api/directions/json?origin=20.637085, -103.430058&destination=20.646384, -103.413641&key=AIzaSyBAvGj8Asdi5OVQpxZAsk5TBOPzBB54WiQ");
 req.responseType = "json";
 req.send();
