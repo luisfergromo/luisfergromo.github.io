a77b04775fddfa2bb4388b21cdc6e0b8744a0b02
luisromo <changoluis@hotmail.com>
2017-09-26 11:49:23 -0500

    ...

--- ws/test.js
+++ ws/test.js
@@ -26,10 +26,10 @@ req.onload = function (e) {
     }
 };
 req.open("GET", "https://maps.googleapis.com/maps/api/directions/json?origin=20.637085, -103.430058&destination=20.646384, -103.413641&key=AIzaSyBAvGj8Asdi5OVQpxZAsk5TBOPzBB54WiQ");
-//req.responseType="json";
+req.responseType = "json";
 //var httpRequest = new httpRequest;
 //req.setRequestHeader('Access-Control-Allow-Origin:', '*');
-req.setRequestHeader('Content-Type', 'application/json');
-//httpReq.setRequestHeader('Access-Control-Allow-Headers', '*');
+//req.setRequestHeader('Content-Type','application/json');
+req.setRequestHeader('Access-Control-Allow-Headers', '*');
 //
 req.send();
