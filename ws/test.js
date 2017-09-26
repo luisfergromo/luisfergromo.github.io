8b8948e254971bf3e40cd6f6db4f0315d09ce62e
luisromo <changoluis@hotmail.com>
2017-09-25 21:18:21 -0500

    html index & test.ts (TypeScript)

--- ws/test.js
+++ ws/test.js
@@ -7,12 +7,14 @@ var message = "Hello Wold"
 console.log(message)
 document.body.innerHTML = message;
 */
+var message = "hello world";
+console.log(message);
+document.body.innerHTML = message;
 var req = new XMLHttpRequest();
-//un listener
 req.onload = function (e) {
     var response = req.response;
     console.log(response);
 };
-req.open("GET", "https://api.github.com/users/luisfergromo");
-//tipo de respuesta
+req.open("GET", "https://api.github.com/users/luisfergromo/repos");
 req.responseType = "json";
+req.send();
--- ws/test.ts
+++ ws/test.ts
@@ -7,12 +7,18 @@ var message = "Hello Wold"
 console.log(message)
 document.body.innerHTML = message;
 */
-var req = new XMLHttpRequest();
-//un listener
-req.onload = function (e) {
+var message = "hello world"
+console.log(message)
+document.body.innerHTML= message
+
+var req = new XMLHttpRequest()
+
+req.onload= function (e) {
+
     var response = req.response;
     console.log(response)
 }
-req.open("GET", "https://api.github.com/users/luisfergromo");
-//tipo de respuesta
-req.responseType = "json";
+
+req.open("GET","https://api.github.com/users/luisfergromo/repos")
+req.responseType="json"
+req.send()
