7abadd3c0ce8a257c86164e5f0211c2c129f4327
luisromo <changoluis@hotmail.com>
2017-09-25 21:02:08 -0500

    html index & test.ts (TypeScript)

--- ws/test.js
+++ ws/test.js
@@ -2,10 +2,16 @@
 var message = "Hello Wold"
 console.log(message)
 document.body.innerHTML = message;
+/*
+var message = "Hello Wold"
+console.log(message)
+document.body.innerHTML = message;
 */
 var req = new XMLHttpRequest();
 //un listener
 req.onload = function (e) {
+    var response = req.response;
+    console.log(response);
 };
 req.open("GET", "https://api.github.com/users/luisfergromo");
 //tipo de respuesta
--- ws/test.ts
+++ ws/test.ts
@@ -2,15 +2,17 @@
 var message = "Hello Wold"
 console.log(message)
 document.body.innerHTML = message;
+/*
+var message = "Hello Wold"
+console.log(message)
+document.body.innerHTML = message;
 */
-
 var req = new XMLHttpRequest();
 //un listener
-req.onload=function(e){
-
+req.onload = function (e) {
+    var response = req.response;
+    console.log(response)
 }
-
-req.open("GET","https://api.github.com/users/luisfergromo")
-
+req.open("GET", "https://api.github.com/users/luisfergromo");
 //tipo de respuesta
-req.responseType="json"
+req.responseType = "json";
