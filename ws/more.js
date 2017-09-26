5f86ffb1d09019437e7d75fd6f6fb960cc136681
luisromo <changoluis@hotmail.com>
2017-09-25 23:47:36 -0500

    .

--- ws/more.js
+++ ws/more.js
@@ -1,17 +1,12 @@
-
 var message = "Hello World";
 console.log(message);
 document.body.innerHTML = message;
-
 var req = new XMLHttpRequest();
-req.onload= function (e) {
-
+req.onload = function (e) {
     var response = req.response;
     console.log(response);
-    console.log(response.login)
+    console.log(response.login);
 };
-
-req.open("GET","https://api.github.com/users/luisfergromo");
-
-req.responseType="json";
+req.open("GET", "https://api.github.com/users/luisfergromo");
+req.responseType = "json";
 req.send();
